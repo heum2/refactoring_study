b625704c3919d592b3ad0f92af62efe81bd64b81
HeumHeum2 <jwh6295@gmail.com>
2021-02-09 09:44:41 +0900

    refactor: volumeCredits 문장 슬라이드하기

--- ch1/statement.js
+++ ch1/statement.js
@@ -3,7 +3,6 @@ import plays from "./plays.js";
 
 function statement(invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `청구내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
@@ -13,6 +12,7 @@ function statement(invoice, plays) {
     }석)\n`;
     totalAmount += amountFor(perf);
   }
+  let volumeCredits = 0;
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
   }
