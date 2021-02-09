cae4b5f7191b81e9d0ce914d736446fe094facbe
HeumHeum2 <jwh6295@gmail.com>
2021-02-09 09:48:41 +0900

    refactor: volumeCredits 값 계산 코드를 totalVolumeCredits 함수로 추출

--- ch1/statement.js
+++ ch1/statement.js
@@ -12,14 +12,23 @@ function statement(invoice, plays) {
     }석)\n`;
     totalAmount += amountFor(perf);
   }
-  let volumeCredits = 0;
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-  }
+  let volumeCredits = totalVolumeCredits();
 
   result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100);
+  }
 
   function amountFor(aPerformance) {
     let result = 0;
@@ -43,8 +52,12 @@ function statement(invoice, plays) {
     return result;
   }
 
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
   }
 
   function volumeCreditsFor(aPerformance) {
@@ -54,14 +67,6 @@ function statement(invoice, plays) {
       result += Math.floor(aPerformance.audience / 5);
     }
     return result;
-  }
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
   }
 }
 
