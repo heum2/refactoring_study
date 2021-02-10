f214ab7edf756af57f5efae70f55a0aa19e5afba
HeumHeum2 <jwh6295@gmail.com>
2021-02-10 10:16:23 +0900

    refactor: renderPlainText의 중첩 함수였던 amountFor를 statement로 옮김

--- ch1/statement.js
+++ ch1/statement.js
@@ -10,33 +10,12 @@ function statement(invoice, plays) {
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
+    result.amount = amountFor(result);
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
-  }
-}
-
-function renderPlainText(data) {
-  let result = `청구내역 (고객명: ${data.customer})\n`;
-  for (let perf of data.performances) {
-    // 청구 내역을 출력한다.
-    result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${
-      perf.audience
-    }석)\n`;
-  }
-
-  result += `총액: ${usd(totalAmount())}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-  return result;
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
   }
 
   function amountFor(aPerformance) {
@@ -60,11 +39,31 @@ function renderPlainText(data) {
     }
     return result;
   }
+}
+
+function renderPlainText(data) {
+  let result = `청구내역 (고객명: ${data.customer})\n`;
+  for (let perf of data.performances) {
+    // 청구 내역을 출력한다.
+    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+  }
+
+  result += `총액: ${usd(totalAmount())}\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  return result;
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100);
+  }
 
   function totalAmount() {
     let result = 0;
     for (let perf of data.performances) {
-      result += amountFor(perf);
+      result += perf.amount;
     }
     return result;
   }
