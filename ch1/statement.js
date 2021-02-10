db7a83ef4e74bcdb66a89f9348d71d60507a8114
HeumHeum2 <jwh6295@gmail.com>
2021-02-10 10:18:26 +0900

    refactor: renderPlainText의 중첩 함수였던 volumeCreditsFor를 statement로 옮김

--- ch1/statement.js
+++ ch1/statement.js
@@ -11,6 +11,7 @@ function statement(invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
@@ -36,6 +37,15 @@ function statement(invoice, plays) {
         break;
       default:
         throw new Error(`알 수 없는 장르 ${aPerformance.play.type}`);
+    }
+    return result;
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === aPerformance.play.type) {
+      result += Math.floor(aPerformance.audience / 5);
     }
     return result;
   }
@@ -71,16 +81,7 @@ function renderPlainText(data) {
   function totalVolumeCredits() {
     let result = 0;
     for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
-    }
-    return result;
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type) {
-      result += Math.floor(aPerformance.audience / 5);
+      result += perf.volumeCredits;
     }
     return result;
   }
