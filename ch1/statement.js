7e0e0a05a848e6539eaede265f339e27d617dca2
HeumHeum2 <jwh6295@gmail.com>
2021-02-10 10:48:44 +0900

    feat: html 출력 함수 생성

--- ch1/statement.js
+++ ch1/statement.js
@@ -18,6 +18,24 @@ function renderPlainText(data) {
   return result;
 }
 
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+  for (let perf of data.performances) {
+    result += ` <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+  }
+  result += `</table>\n`;
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+  return result;
+}
+
 function usd(aNumber) {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
@@ -27,3 +45,5 @@ function usd(aNumber) {
 }
 
 console.log(statement(invoices[0], plays));
+const app = document.querySelector(".app");
+app.innerHTML = htmlStatement(invoices[0], plays);
